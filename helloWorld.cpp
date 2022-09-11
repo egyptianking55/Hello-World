a7dac06cd8165d29b375d0ef7c51bdaa00914ade
Egyptianking55 <egyptianking55@gmail.com>
2022-09-10 17:31:52 -0700

    Update Hello->Bye.

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main () {
 
     //why are you reading this?  It's just Hello World.
-    cout << "Hello World.\n";
+    cout << "Good World.\n";
 
     return 0;
     //Why are you still here?  The program's over.  Go home.
