2b6b11226fb9929a54d41637947e293f6cd179cc
Egyptianking55 <egyptianking55@gmail.com>
2022-09-10 17:14:49 -0700

    Create initial Hello World file; Test VScode, Git, Sourcetree.

---
+++ helloWorld.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+    //why are you reading this?  It's just Hello World.
+    cout << "Hello World.\n";
+
+    return 0;
+    //Why are you still here?  The program's over.  Go home.
+}
